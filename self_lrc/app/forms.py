5ebca5da5d6e370f55fa26278f3da74751bed022
Prabesh01 <prabesh01@pm.me>
2025-03-21 12:25:38 +0545

    add user interface for admin. change password for others

--- self_lrc/app/forms.py
+++ self_lrc/app/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
 
 class CustomPasswordChangeForm(PasswordChangeForm):
     old_password = forms.CharField(
@@ -14,3 +16,35 @@ class CustomPasswordChangeForm(PasswordChangeForm):
         label="Confirm New Password",
         widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm New Password'})
     )
+
+
+class AddUserForm(forms.Form):
+    username = forms.CharField(
+        label="username",
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'username for New user '})
+    )
+    password = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'})
+    )
+
+    def __init__(self, user=None, *args, **kwargs):
+        super(AddUserForm, self).__init__(*args, **kwargs)    
+
+    def save(self):
+        username = self.cleaned_data['username']
+        password = self.cleaned_data['password']
+        
+        # Create new user
+        user = User.objects.create_user(
+            username=username,
+            password=password,
+            is_staff=True
+        )        
+        try:
+            allow_group = Group.objects.get(name='allow')
+            user.groups.add(allow_group)
+        except Group.DoesNotExist:
+            pass
+            
+        return user
--- self_lrc/app/views.py
+++ self_lrc/app/views.py
@@ -4,20 +4,29 @@ from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth import update_session_auth_hash
-from .forms import CustomPasswordChangeForm
+from .forms import CustomPasswordChangeForm, AddUserForm
 
 def get_home(request):
     if not request.user.is_authenticated: return redirect('/admin/login/')
-    if request.method != 'POST':
-        form = CustomPasswordChangeForm(request.user)
-        return render(request, 'change_passwd.html', {'form': form})
+    
+    if request.user.is_superuser: 
+        site='add_user.html'
+        form_f = AddUserForm
+        success="User added successfully!"
     else: 
-        form = PasswordChangeForm(request.user, request.POST)
+        site='change_passwd.html'
+        form_f = CustomPasswordChangeForm
+        success="Your password was successfully updated! Login with new credentials!"
+    if request.method != 'POST':
+        form = form_f(request.user)
+        return render(request, site, {'form': form})
+    else: 
+        form = form_f(request.user, request.POST)
         if form.is_valid():
             user = form.save()
             # update_session_auth_hash(request, user)
-            messages.success(request, 'Your password was successfully updated! Login with new credentials!')
+            messages.success(request, success)
             return redirect('get_home')
         else:
             messages.error(request, 'Please correct the error below.')
-            return render(request, 'change_passwd.html', {'form': form})
+            return render(request, site, {'form': form})
